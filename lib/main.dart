1955712c2e6c419786116e6e4e93289115171121
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-16 15:29:55 +0600

    statefull

--- lib/main.dart
+++ lib/main.dart
@@ -37,24 +37,47 @@ class MyApp extends StatelessWidget {
 //   }
 // }
  // reading Value Anather way
- class HomePage extends StatelessWidget {
+//  class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+    
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Center(child: Text('Material App Bar')),
+//         ),
+//         body:  Center(
+//           child: Consumer(builder: (context, ref, child) {
+//             final name = ref.watch(nameProvider);
+//             return Center(
+//                           child: Text(name),
+//                         );
+//           },),
+//         ),
+//       );
+//   }
+// }
+
+// reading Value from statefull widgets
+
+ class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    
+    final name = ref.watch(nameProvider);
     return Scaffold(
         appBar: AppBar(
           title: const Center(child: Text('Material App Bar')),
         ),
         body:  Center(
-          child: Consumer(builder: (context, ref, child) {
-            final name = ref.watch(nameProvider);
-            return Center(
-                          child: Text(name),
-                        );
-          },),
-        ),
-      );
+                        child: Text(name),
+      )  );
   }
 }
