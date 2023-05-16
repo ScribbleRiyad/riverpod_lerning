874d59b5fef5a024e6d581fdd3e680debfb5bd71
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-16 15:00:28 +0600

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() => runApp(const MyApp());
+final nameProvider = Provider<String>((ref) => "hello Scribble",);
+
+
+void main() => runApp(const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Material App',
-      home: Scaffold(
+      home: HomePage()
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           title: const Text('Material App Bar'),
         ),
         body: const Center(
           child: Text('Hello World'),
         ),
-      ),
-    );
+      );
   }
 }
