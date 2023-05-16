5f2ecfcad46fa5c6937e5b4489b603a887eed5f7
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-16 15:21:29 +0600

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final nameProvider = Provider<String>((ref) => "hello Scribble",);
+final nameProvider = Provider<String>((ref) => "Provider Scribble Riyad",);
 
 
 void main() => runApp(const ProviderScope(child: MyApp()));
@@ -12,22 +12,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
       home: HomePage()
     );
   }
 }
-class HomePage extends StatelessWidget {
+
+ // reading Value this way
+// class HomePage extends ConsumerWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final name = ref.watch(nameProvider);
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Center(child: Text('Material App Bar')),
+//         ),
+//         body:  Center(
+//           child: Text(name),
+//         ),
+//       );
+//   }
+// }
+ // reading Value Anather way
+ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Center(child: Text('Material App Bar')),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:  Center(
+          child: Consumer(builder: (context, ref, child) {
+            final name = ref.watch(nameProvider);
+            return Center(
+                          child: Text(name),
+                        );
+          },),
         ),
       );
   }
