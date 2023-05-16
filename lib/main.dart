6bea52409f312eebc11930512c5e649bbae61ea1
ScribbleRiyad <scribbleriyad@gmail.com>
2023-05-16 16:44:36 +0600

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final nameProvider = Provider<String>((ref) => "Riverpod Scribble Riyad",);
+final counterProvider = StateProvider<int>((ref) => 0,);
 
 
 void main() => runApp(const ProviderScope(child: MyApp()));
@@ -20,48 +20,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
- // reading Value this way
-// class HomePage extends ConsumerWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context, WidgetRef ref) {
-//     final name = ref.watch(nameProvider);
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Center(child: Text('Material App Bar')),
-//         ),
-//         body:  Center(
-//           child: Text(name),
-//         ),
-//       );
-//   }
-// }
- // reading Value Anather way
-//  class HomePage extends StatelessWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: const Center(child: Text('Material App Bar')),
-//         ),
-//         body:  Center(
-//           child: Consumer(builder: (context, ref, child) {
-//             final name = ref.watch(nameProvider);
-//             return Center(
-//                           child: Text(name),
-//                         );
-//           },),
-//         ),
-//       );
-//   }
-// }
-
-// reading Value from statefull widgets
-
  class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
@@ -76,9 +34,9 @@ class _HomePageState extends ConsumerState<HomePage> {
 void initState() {
    
   super.initState();
-  final name = ref.read(nameProvider);
+
    
-   print(name);
+
   
 }
 
@@ -86,13 +44,13 @@ void initState() {
 
   @override
   Widget build(BuildContext context) {
-    final name = ref.watch(nameProvider);
+   
     return Scaffold(
         appBar: AppBar(
           title: const Center(child: Text('Material App Bar')),
         ),
-        body:  Center(
-                        child: Text(name),
+        body:  const Center(
+                        child: Text(""),
       )  );
   }
 }
